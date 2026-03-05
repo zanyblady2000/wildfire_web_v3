b989e1c0ca830e210f2f7bf26fe1a053339c9af5
zanyblady2000 <zanyblady2000@gmail.com>
2026-03-04 19:41:51 -0500

    Initialize app.py with model loading and inputs

---
+++ app.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import streamlit as st
+import joblib
+import ploty.express as px
+
+rfc = joblib.load('rfc_model.joblib')
+scaler = joblib.load('scaler.joblib')
+
+def Inputs():
+  st.sidebar.header = ("Inputs For Prediction")
+  
