282905b2b536e818ce18c4f02485264721c7cef6
zanyblady2000 <zanyblady2000@gmail.com>
2026-03-04 20:22:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ if st.button('Predict Wildfire Within Area'):
   prediction_label = {0: 'Low Chance of Fire', 1: 'High Chance of Fire'}
 
   st.subheader("Predicted Result:")
-  fire_risk_label = "High" if prediction == 1 else "Low"
+  fire_risk_label = "High" if prediction[0] == 1 else "Low"
 
   if fire_risk_label == "High":
       st.error(f"Predicted Fire Risk: **{fire_risk_label}**")
