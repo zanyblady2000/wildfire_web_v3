9ea5e02cc432805d68f376aa47613b27421af9c2
zanyblady2000 <zanyblady2000@gmail.com>
2026-03-04 19:54:17 -0500

    Add sliders for wind speed, humidity, and location inputs

--- app.py
+++ app.py
@@ -10,4 +10,21 @@ def Inputs():
   st.sidebar.header = ("Inputs For Prediction")
 
   temp = st.sidebar.slider = ('Temperature (C*)', 15, 35, 0)
+  wind_speed = st.sidebar.slider = ('Wind_Speed (Km/h)', 0, 50, 0)
+  humidity = st.sidebar.slider = ('Humidity (%)', 0, 100, 0)
+  LDSR = st.sidebar.slider = ('Last Day Since Rain)', 0, 7, 0)
+
+  st.sidebar.subheader = ("Location Inputs")
+  lat = st.sidebar.slider('Latitude', 50, 59, 55)
+  long = st.sidebar.slider('Longitude', -124, -113, -118)
+
+   data = {
+          'temp': temp, 
+          'humidity': humidity, 
+          'windspeed': windspeed,
+          'lat': lat, 
+          'long': long
+      }
+      return pd.DataFrame(data, index=[0])
+
   
