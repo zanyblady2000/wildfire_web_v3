674a84e0e4de8118aaa28c2b65daa083f7ec4181
zanyblady2000 <zanyblady2000@gmail.com>
2026-03-04 19:42:58 -0500

    Fix import statement for plotly.express

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import streamlit as st
 import joblib
-import ploty.express as px
+import plotly.express as px
 
 rfc = joblib.load('rfc_model.joblib')
 scaler = joblib.load('scaler.joblib')
