0c8c719c01253ab04a4cc170214548edac79c8b0
zanyblady2000 <zanyblady2000@gmail.com>
2026-03-26 07:28:57 -0400

    Update app.py

--- app.py
+++ app.py
@@ -83,7 +83,7 @@ with tab1:
     color_discrete_map={"High Risk": 'red', "Low Risk": 'green'},
     height=500,
     hover_data=[Aug_df.index],
-    labels={"0": "day"},
+    labels={"_0": "day"},
     mapbox_style="open-street-map"
   )
 
