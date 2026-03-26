576b43ebd6b36889eeeac1dc3414edd1b8329361
zanyblady2000 <zanyblady2000@gmail.com>
2026-03-25 20:47:27 -0400

    Update app.py

--- app.py
+++ app.py
@@ -112,7 +112,7 @@ if st.button('Predict Wildfire Within Area'):
      st.success(f"Predicted Fire Risk: **{fire_risk_label}**")
 
   map_df = input_df.copy()
-  map_df['fire_risk_label'] = fire_risk_label[prediction.item()]
+  map_df['fire_risk_label'] = fire_risk_label[prediction]
         
        
   fig = px.scatter_mapbox(
